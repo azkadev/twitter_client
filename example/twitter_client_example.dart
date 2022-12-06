dbffa567a2a3a0cf0a86285e19d9b24b787021da
￴ <>
2022-12-06 13:39:42 +0700

    updated

---
+++ example/twitter_client_example.dart
@@ -0,0 +1,6 @@
+import 'package:twitter_client/twitter_client.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/twitter_client.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library twitter_client;
+
+export 'src/twitter_client_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
