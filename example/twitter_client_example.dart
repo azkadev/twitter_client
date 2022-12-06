5986efae4879d9f16a0f71b548b8feeaf55d8db9
￴ <>
2022-12-06 13:49:57 +0700

    update

--- example/twitter_client_example.dart
+++ example/twitter_client_example.dart
@@ -1,6 +1,30 @@
+// Copyright 2022 Kato Shinya. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided the conditions.
+
+import 'dart:async';
+import 'dart:io';
+
 import 'package:twitter_client/twitter_client.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+Future<void> main() async {
+  TwitterClient twitterClient = TwitterClient( 
+  );
+
+  try {
+    var message = """
+Hallow Dengan Lpm RolePlayer | LPM RP Disini
+
+Test Auto Post
+
+#lpmroleplayer #lpm #lpmrp #roleplayer #roleplayerindonesia #Roleplayerent #lpmpromote #bts #blackpink #RedVelvet #NCT127
+""";
+    message += "\n${DateTime.now().millisecondsSinceEpoch}";
+    var res = await twitterClient.client().tweets.createTweet(
+          text: message,
+        );
+    print(res);
+  } catch (e, stack) {
+    print(e);
+  }
 }
--- lib/twitter_client.dart
+++ lib/twitter_client.dart
@@ -1,8 +1,66 @@
 /// Support for doing something awesome.
 ///
 /// More dartdocs go here.
+// ignore_for_file: non_constant_identifier_names
+
 library twitter_client;
 
+import 'package:twitter_api_v2/twitter_api_v2.dart' as twtter_api;
+export 'package:twitter_api_v2/twitter_api_v2.dart';
 export 'src/twitter_client_base.dart';
 
 // TODO: Export any libraries intended for clients of this package.
+
+class TwitterClient {
+  late String bearer_token;
+  late String consumer_key;
+  late String consumer_secret;
+  late String access_token;
+  late String access_token_secret;
+  TwitterClient({
+    required String bearerToken,
+    required String consumerKey,
+    required String consumerSecret,
+    required String accessToken,
+    required String accessTokenSecret,
+  }) {
+    bearer_token = bearerToken;
+    consumer_secret = consumerSecret;
+    consumer_key = consumerKey;
+    access_token = accessToken;
+    access_token_secret = accessTokenSecret;
+  }
+
+  twtter_api.TwitterApi client({
+    String? bearerToken,
+    String? consumerKey,
+    String? consumerSecret,
+    String? accessToken,
+    String? accessTokenSecret,
+  }) {
+    bearerToken ??= bearer_token;
+    consumerKey ??= consumer_key;
+    consumerSecret ??= consumer_secret;
+    accessToken ??= access_token;
+    accessTokenSecret ??= access_token_secret;
+
+    return twtter_api.TwitterApi(
+      bearerToken: bearerToken,
+      oauthTokens: twtter_api.OAuthTokens(
+        consumerKey: consumerKey,
+        consumerSecret: consumerSecret,
+        accessToken: accessToken,
+        accessTokenSecret: accessTokenSecret,
+      ),
+      retryConfig: twtter_api.RetryConfig.ofExponentialBackOffAndJitter(
+        maxAttempts: 5,
+        onExecute: (event) => print(
+          'Retry after ${event.intervalInSeconds} seconds... '
+          '[${event.retryCount} times]',
+        ),
+      ),
+    );
+  }
+
+  
+}
